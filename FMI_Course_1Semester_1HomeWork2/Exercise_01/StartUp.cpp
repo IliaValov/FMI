cdef9f6ec3f5033325d40a6ac9f544b7f7dd7e10
IliqVulov <iliqvulov@abv.bg>
2019-12-01 22:49:28 +0200

    Added HomeWork and first exercise

---
+++ FMI_Course_1Semester_1HomeWork2/Exercise_01/StartUp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int SUBSTACT_SYMBOLBY_A = 'A' - 1;
+
+	char* symbols = new char[4]();
+
+	cout << "Enter symbol "; cin >> symbols;
+
+	int firstSymbolInInt = symbols[0] - SUBSTACT_SYMBOLBY_A ;
+
+	int column = firstSymbolInInt;
+
+	if (symbols[2] >= 'A' && symbols[2] <= 'Z') {
+		column =  (firstSymbolInInt * 676)+ ((symbols[1] - SUBSTACT_SYMBOLBY_A) * 26)  + ( symbols[2] - SUBSTACT_SYMBOLBY_A);
+	}
+	else if (symbols[1] >= 'A' && symbols[1] <= 'Z') {
+		column = (firstSymbolInInt * 26) + (symbols[1] - SUBSTACT_SYMBOLBY_A);
+	}
+
+	cout << column;
+
+	delete[] symbols;
+	return 0;
+}
